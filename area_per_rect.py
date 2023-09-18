07adcd60195672b4e4f294a6f6d3d7e951e3988b
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-18 23:10:23 +0000

    added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created By: Finn Kitor
+# Date: September 18th, 2023
+# This program is about finding the area and perimeter of a rectangle
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 3cm and width 3cm:")
+    print("")
+    print("Area = {} cm^2".format(3 * 3))
+    print("Perimeter = {} cm".format(2 * (3 + 3)))
+
+
+if __name__ == "__main__":
+    main()
