cffec678eb0ea42f5ce5ce09928e316f5642c51d
Finn Kitor <finn.patrick.kitor@stu.ocsb.ca>
2023-09-18 22:51:07 +0000

    added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Finn Kitor
+# Date: September 18th, 2023
+# This program ia about math operators.
+def main():
+    print("8 + 2 = {}".format(8 + 2))
+    print("7 - 1 = {}".format(7 - 1))
+    print("6 / 2 = {}".format(6 / 2))
+    print("8 - 2 / 2 = {}".format(8 - 2 / 2))
+    print("2^8 = {}".format(2**8))
+    print("1 / 5 = {}".format(1 / 5))
+
+
+if __name__ == "__main__":
+    main()
